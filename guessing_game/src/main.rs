124a4d99199dd5e145526b3d49e7bbbd018b9449
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-05-19 21:12:46 -0300

    .

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,34 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Adivinhe um numero!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("O numero secreto é: {}", secret_number);
+    loop {
+        println!("Insira um numero");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Você adivinhou: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Você errou! O numero secreto é maior"),
+            Ordering::Greater => println!("Você errou! O numero secreto é menor"),
+            Ordering::Equal => {
+                println!("Você acertou!");
+                break;
+            }
+        }
+    }
+}
