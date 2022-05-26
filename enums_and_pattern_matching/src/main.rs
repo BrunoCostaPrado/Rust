1e1816255d5828614e008babacb71f4f231ef12e
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-05-26 04:20:19 -0300

    edit

--- enums_and_pattern_matching/src/main.rs
+++ enums_and_pattern_matching/src/main.rs
@@ -1,10 +1,11 @@
+#![allow(non_snake_case)]
 fn main() {
     let some_value = Some(3);
     match some_value {
         Some(3) => println!("three"),
         _ => (),
     }
-    
+
     if let Some(3) = some_value {
         println!("three");
     }
