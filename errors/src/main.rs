7ecc5cade54a8691b249e48d8fd722b44d490143
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-06-13 23:09:22 -0300

    edit

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,4 +1,16 @@
-fn main() {
-    panic!("crash and burn");
-    
+// CMD :set RUST_BACKTRACE=1
+//POWERSHELL :$Env:RUST_BACKTRACE=1
+
+// use std::fs::{self};
+// use std::io;
+
+// fn read_usename_from_file() -> Result<String, io::Error> {
+//     fs::read_to_string("hello.txt")
+// }
+
+use std::error::Error;
+use std::fs::File;
+fn main() -> Result<(), Box<dyn Error>> {
+    let f = File::open("hello.txt")?;
+    Ok(())
 }
