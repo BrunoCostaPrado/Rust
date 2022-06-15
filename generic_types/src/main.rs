e471ee9772676a10661440d5289963258054427e
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-06-14 23:41:13 -0300

    edit

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let largest = get_largest(number_list);
+    println!("O maior numero é {}", largest);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let largest = get_largest(number_list);
+
+    println!("O maior numero é {}", largest);
+}
+
+fn get_largest(number_list: Vec<i32>) -> i32 {
+    let mut largest = number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
 }
